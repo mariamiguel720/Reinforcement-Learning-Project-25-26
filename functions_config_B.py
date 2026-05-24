96794f0fc1599b362f18871384addf4c271533af
Catarina Mendinhas <ccmendinhas04@gmail.com>
2026-05-24 03:08:03 +0100

    PPO tests

--- functions_config_B.py
+++ functions_config_B.py
@@ -376,8 +376,8 @@ def compute_gae(rewards, values, dones, lam=0.95):
     values_np = [v.item() for v in values] + [0.0]  # bootstrap with 0
 
     for t in reversed(range(len(rewards))):
-        delta = rewards[t] + gamma * values_np[t+1] * (1 - dones[t]) - values_np[t]
-        gae   = delta + gamma * lam * (1 - dones[t]) * gae
+        delta = rewards[t] + GAMMA * values_np[t+1] * (1 - dones[t]) - values_np[t]
+        gae   = delta + GAMMA * lam * (1 - dones[t]) * gae
         advantages.insert(0, gae)
 
     return advantages
